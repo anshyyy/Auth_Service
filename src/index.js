47d50483f40a3c1ffe90f209da8c5a328c5da0e2
anshyyy <anshuman9998@gmail.com>
2022-12-23 17:21:04 +0530

    Added project skeleton

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const { PORT } = require('./config/serverConfig');
+
+const prepareAndStartServer = () => {
+    
+    app.listen(PORT,async()=>{
+
+        console.log(`Server Started at ${PORT} ` )
+    });
+}
+
+prepareAndStartServer();
