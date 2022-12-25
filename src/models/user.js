501a59f2e958e4b3822232f3e673f2545bb264fc
anshyyy <anshuman9998@gmail.com>
2022-12-25 23:31:25 +0530

    Added signUp logic with encryption

--- src/models/user.js
+++ src/models/user.js
@@ -2,6 +2,8 @@
 const {
   Model
 } = require('sequelize');
+const {SALT} = require('../config/serverConfig');
+const bcrypt = require('bcrypt');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -31,6 +33,11 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'User',
+  });
+
+  User.beforeCreate((user)=>{
+    const encryptedPassword = bcrypt.hashSync(user.password,SALT);
+    user.password = encryptedPassword
   });
   return User;
 };
